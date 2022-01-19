28e2492d340110f0c3c178ce3528e10ea541e536
HwangSaw <tinkle9696@naver.com>
2022-01-19 15:19:15 +0900

    프로젝트 정리

---
+++ ClientProjects/Clientcode/Lesson12Thread1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ClientCode 
+{ 
+
+    class Test
+    {
+        public void Print()
+        {
+            Console.WriteLine("Test!");
+        }
+    }
+    class Lesson12Thread1
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            Thread th = new Thread(new ThreadStart(test.Print));
+            th.Start();
+        }
+    }
+}
